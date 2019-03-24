6fc25207446795253799779edcaf43589542b747
Peter van Heusden <pvh@sanbi.ac.za>
2019-03-24 11:48:21 +0200

    * draft of conda package * add copyright info

--- tb_variant_filter/filters/__init__.py
+++ tb_variant_filter/filters/__init__.py
@@ -1,4 +1,22 @@
-# these filter modules are very closely tied to the command line interface (tb_variant_filter.cli)
+# Copyright (C) 2019  Peter van Heusden <pvh@sanbi.ac.za>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# TODO:
+# these filter modules are very closely tied to the command line interface (tb_variant_filter.cli),
+# to the extent that they do not stand alone well, which is something that should be changed
+# in a future refactor
 from abc import abstractmethod
 import argparse
 from typing import List
